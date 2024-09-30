7be9e78381dd125db0f534e7de0478fd66a65448
Vitali Knyrevich <knyrevichvitali@gmail.com>
2024-09-30 03:08:23 +0300

    new method and parameters were added

--- source/product.h
+++ source/product.h
@@ -5,12 +5,15 @@ class product
 {
     string name;
     float price;
+    float rating = 0;
+    int number_of_votes = 0;
 public:
     void read() const;
     void read_only_names(int) const;
     void set_name();
     void reset_name();
     float set_price();
+    void rate_the_course();
     void renew(product*, int&);
     void remove_course(product* courses, int& amount, int order);
 };
