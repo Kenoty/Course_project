76fa5bb71425df40294785d9cee5030a93537739
Vitali Knyrevich <knyrevichvitali@gmail.com>
2024-10-02 00:33:11 +0300

    interface.h was added

---
+++ source/interface.h
@@ -0,0 +1,8 @@
+#include "product.h"
+
+void routine();
+void create_the_product(product*, int&);
+void output_product_info(product*, int);
+void rate_product(product*, int);
+void update_info(product*,int);
+void delete_product(product*, int&);
