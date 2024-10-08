c23533f6076cec2017c92f4833cf35f69c8421db
Vitali Knyrevich <knyrevichvitali@gmail.com>
2024-10-08 09:22:12 +0300

    sonar bug fix

--- source/product.h
+++ source/product.h
@@ -8,12 +8,8 @@ class Product
     float rating = 0;
     int number_of_votes = 0;
 public:
-    Product(std::string name = " ", float price = 0, float rating = 0, int number_of_votes = 0)
+    Product()
     {
-        this->name = name;
-        this->price = price;
-        this->rating = rating;
-        this->number_of_votes = number_of_votes;
     }
     void read() const;
     void read_only_names(unsigned int) const;
