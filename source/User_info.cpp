2424654d6f19f3777aa14291e5af673117052de8
Vitali Knyrevich <knyrevichvitali@gmail.com>
2024-11-14 13:03:32 +0300

    user_info class was renamed

--- source/User_info.cpp
+++ source/User_info.cpp
@@ -1,41 +1,41 @@
 #include "user_info.h"
 
-void User_info::set_fname(const std::string_view value)
+void UserInfo::set_fname(const std::string_view value)
 {
 	this->first_name = value;
 }
 
-void User_info::set_sname(const std::string_view value)
+void UserInfo::set_sname(const std::string_view value)
 {
 	this->second_name = value;
 }
 
-void User_info::set_lname(const std::string_view value)
+void UserInfo::set_lname(const std::string_view value)
 {
 	this->last_name = value;
 }
 
-void User_info::set_email(const std::string_view value)
+void UserInfo::set_email(const std::string_view value)
 {
 	this->email = value;
 }
 
-void User_info::set_phone_number(const std::string_view value)
+void UserInfo::set_phone_number(const std::string_view value)
 {
 	this->phone_number = value;
 }
 
-void User_info::set_user_password(const std::string_view value)
+void UserInfo::set_user_password(const std::string_view value)
 {
 	this->user_password = value;
 }
 
-void User_info::set_role(const std::string_view value)
+void UserInfo::set_role(const std::string_view value)
 {
 	this->role = value;
 }
 
-std::string User_info::get_role()
+std::string UserInfo::get_role()
 {
 	return this->role;
 }
