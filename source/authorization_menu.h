2ef3eaea45277bf9553e6968c189e02b8b572c02
Vitali Knyrevich <knyrevichvitali@gmail.com>
2024-11-14 13:08:45 +0300

    function related to auth_menu was added

--- source/authorization_menu.h
+++ source/authorization_menu.h
@@ -2,12 +2,14 @@
 #include "menu_interface.h"
 #include "user_info.h"
 
-class Authorization_menu: public IMenu, public Database
+class AuthorizationMenu: public IMenu, public Database
 {
 	const std::string table_name = "users";
 public:
 	void registration();
-	void login(User_info&);
+	void login(UserInfo&);
 	int validation(const std::string* field_names, const std::string* values, const int length) const;
 	void output_menu();
+	//void choose_option(User_info&);
+	void choose_option();
 };
